21aa66ada114a2f42780a8f2a61a917b2349ab64
Shahd <shahad9mubarak@gmail.com>
2026-04-23 11:24:04 +0400

    PUSU TO STACK

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -5,6 +5,7 @@ public class BrowserHistorySimulator {
         Stack<String> URL = new Stack<>();
         URL.push("file:///C:/Users/codel/Downloads/Java%20Stack.pdf");
         URL.push("https://app.clickup.com/90182612218/v/b/6-901817329466-2");
+        URL.push("https://github.com/Shahd-geo/JavaStack");
 
 
 
