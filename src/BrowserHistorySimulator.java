d6e66603e90af53f2b40119bb7e3692140ec09dd
Shahd <shahad9mubarak@gmail.com>
2026-04-23 11:23:05 +0400

    PUSU TO STACK

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -3,6 +3,8 @@ import java.util.Stack;
 public class BrowserHistorySimulator {
     static void main(String[] args) {
         Stack<String> URL = new Stack<>();
+        URL.push("file:///C:/Users/codel/Downloads/Java%20Stack.pdf");
+
 
     }
 }
