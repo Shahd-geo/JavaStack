401260e14f95eeec237a535b563bcbc93d444744
Shahd <shahad9mubarak@gmail.com>
2026-04-23 10:55:59 +0400

    PUSH ELEMENTS TO STACK

--- src/EmployeeStackSearch.java
+++ src/EmployeeStackSearch.java
@@ -3,6 +3,11 @@ import java.util.Stack;
 public class EmployeeStackSearch {
     static void main(String[] args) {
         Stack<String> STACK=new Stack<>();
+        STACK.push("READ");
+        STACK.push("WRITE");
+        STACK.push("ENTER");
+        STACK.push("LEAVE");
+        STACK.push("WORK");
 
     }
 }
