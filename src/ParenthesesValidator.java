9c5a8ac233a77028e2bb9807963a2aa2b35a9955
Shahd <shahad9mubarak@gmail.com>
2026-04-23 11:46:25 +0400

    creat test string

--- src/ParenthesesValidator.java
+++ src/ParenthesesValidator.java
@@ -3,6 +3,11 @@ import java.util.Stack;
 public class ParenthesesValidator {
     static void main(String[] args) {
         Stack<String>  parentheses=new Stack<>();
+        parentheses.push("[]");
+        parentheses.push("()");
+        parentheses.push("[]()");
+        parentheses
+
 
     }
 }
