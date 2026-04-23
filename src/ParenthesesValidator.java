07b76016c87fd3d8d3fff2dafbc59740a17f5923
Shahd <shahad9mubarak@gmail.com>
2026-04-23 12:07:21 +0400

    MAKE SURE NOT EQUALS THE CLOSED PARENTHESES

--- src/ParenthesesValidator.java
+++ src/ParenthesesValidator.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class ParenthesesValidator {
     static void main(String[] args) {
-        Stack<String>  parentheses=new Stack<>();
+        Stack<String> parentheses = new Stack<>();
         parentheses.push("[]");
         parentheses.push("()");
         parentheses.push("[]()");
@@ -12,17 +12,22 @@ public class ParenthesesValidator {
 
 
     }
-    public static Boolean  isBalanced(String str){
-        Stack<Character> STACK=new Stack<>();
-        for (int i = 0 ; i < str.length();i++){
-            char ch =str.charAt(i);
-            if (ch==')'||ch==']'||ch=='}'){
+
+    public static Boolean isBalanced(String str) {
+        Stack<Character> STACK = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == ')' || ch == ']' || ch == '}') {
                 STACK.push(ch);
-            } else if (ch=='('||ch=='{'||ch=='[') {
-                if (STACK.isEmpty()){
-                return false;
+            } else if (ch == '(' || ch == '{' || ch == '[') {
+                if (STACK.isEmpty()) {
+                    return false;
+
+                }
+                char top =STACK.pop();
 
             }
-
         }
-    }}
+        return true;
+    }
+}
