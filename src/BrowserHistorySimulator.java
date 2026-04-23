0e6ba471ab23c68392b64f74964249cfa80f5825
Shahd <shahad9mubarak@gmail.com>
2026-04-23 11:23:38 +0400

    PUSU TO STACK

--- src/BrowserHistorySimulator.java
+++ src/BrowserHistorySimulator.java
@@ -4,6 +4,8 @@ public class BrowserHistorySimulator {
     static void main(String[] args) {
         Stack<String> URL = new Stack<>();
         URL.push("file:///C:/Users/codel/Downloads/Java%20Stack.pdf");
+        URL.push("https://app.clickup.com/90182612218/v/b/6-901817329466-2");
+
 
 
     }
