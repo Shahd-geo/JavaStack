34ee9efdca144876293e494fe44919d4f116f8fd
Shahd <shahad9mubarak@gmail.com>
2026-04-23 11:16:49 +0400

    ADD  iterator

--- src/EmployeeStackSearch.java
+++ src/EmployeeStackSearch.java
@@ -21,6 +21,9 @@ public class EmployeeStackSearch {
             System.out.println(EMPLOYYE);
         }
         Iterator<String>IT=STACK.iterator();
+        while (IT.hasNext()){
+
+        }
 
 
 
