cecd340c03e6532f9e32c182497bfbd3f5823a46
Shahd <shahad9mubarak@gmail.com>
2026-04-23 12:34:10 +0400

    CRETE string to test

--- src/StringReversalAndPalindrome.java
+++ src/StringReversalAndPalindrome.java
@@ -2,9 +2,14 @@ import java.util.Stack;
 
 public class StringReversalAndPalindrome {
     static void main(String[] args) {
+        Stack<String>STACK=new Stack<>();
+        STACK.push("add");
+        STACK.push("hello");
+
     }
     public static String oreverseString(String str){
-        Stack<String>STACK=new Stack<>();
+
+
 
     }
 }
