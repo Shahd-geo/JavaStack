2079d6ab85647cc2ddb308149682c68f9888bd7d
Shahd <shahad9mubarak@gmail.com>
2026-04-23 10:47:37 +0400

    PUSH ELEMENTS TO STACK

--- src/EmployeeStackDemo.java
+++ src/EmployeeStackDemo.java
@@ -4,6 +4,9 @@ public class EmployeeStackDemo {
     static void main(String[] args) {
         Stack<String>STACK=new Stack<>();
         STACK.push("READ");
+        STACK.push("WRITE");
+        STACK
+
 
 
     }
