3cc37f2987d2b687629180283c2c7048e417d391
Shahd <shahad9mubarak@gmail.com>
2026-04-26 10:52:48 +0400

    Add html cond

--- src/HtmlTagValidator.java
+++ src/HtmlTagValidator.java
@@ -4,9 +4,11 @@ public class HtmlTagValidator {
     static void main(String[] args) {
 
 }
-public static boolean validateHtmlTags(String str){
+public static boolean validateHtmlTags(String html){
         Stack<String>STACK=new Stack<>();
         for (int i =0; i<str.length();i++){
+            if (html.charAt(i) == '<') {
+        }
 
 }
 
