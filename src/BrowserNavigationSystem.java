17b731dbcdcb5a3b45f0416b9c1daca16734214a
Shahd <shahad9mubarak@gmail.com>
2026-04-26 16:30:43 +0400

    Task 12: Browser Navigation System with Back and Forward Stacks

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -1,36 +1,91 @@
 import java.util.Stack;
 
-public class BrowserNavigationSystem {
+        public class BrowserNavigationSystem {
+            static Stack<String> back = new Stack<>();
+            static Stack<String> forward = new Stack<>();
+            static String currentPage = "";
+            static int max = 10;
 
-    public static void main(String[] args) {
-        Stack<String>back=new Stack<>();
-        Stack<String>forwared=new Stack<>();
-        String PAGE = "";
-        int max = 10;
-        visitPage("linkedIn.com");
-        visitPage("gitHub.com");
-        visitPage("zoom.com");
-        visitPage("googlemeet.com");
+            public static void main(String[] args) {
 
-        goBack();
-        goBack();
-        goForward();
-        visitPage("GEE.com");
+                visitPage("Codline");
+                visitPage("linkedIn");
+                visitPage("ZOOM");
+                visitPage("GEE");
+                visitPage("Gemail");
 
-        goForward();
-        goBack();
-        showHistory();
+                goBack();
+                goBack();
+                goForward();
+                visitPage("stackoverflow.com");
+
+                goForward();
+                goBack();
+                showHistory();
 
 
-    }
-    static void visitPage(String url) {
-        if (back.size() >= max) {
-            System.out.println("History limit reached ");
-            back.remove(0);
+            }
+
+            static void visitPage(String url) {
+                //limit back history to max only
+                if (back.size() >= max) {
+                    System.out.println("History limit reached ");
+                    back.remove(0);
+
+                }
+                if (!currentPage.isEmpty()) {
+                    back.push(currentPage);
+
+                }
+                //clear forwared stack when visiting new pages
+                forward.clear();
+                //set new cuurent page
+                currentPage = url;
+                System.out.println("Visited  : " + url);
+
+
+            }
+
+            public static void goBack() {
+                // edge case when  nothing to go back to
+                if (back.isEmpty()) {
+                    System.out.println("Cannot go back - no history");
+                    return;
+                }
+                forward.push(currentPage);
+                currentPage = back.pop();
+                System.out.println("Go Back  : " + currentPage);
+
+            }
+
+            // go forward to next page
+            static void goForward() {
+                //edge case when  nothing to go forward to
+                if (forward.isEmpty()) {
+                    System.out.println("Cannot go forward - no forward history");
+                    return;
+
+                }
+                // push current page to back stack
+                back.push(currentPage);
+                // pop last page
+                currentPage = forward.pop();
+                System.out.println("Go Forward : " + currentPage);
+
+            }
+
+            public static void showCurrentPage() {
+                System.out.println("Current Page : " + currentPage);
+                System.out.println("Back         : " + back);
+                System.out.println("Forward      : " + forward);
+
+            }
+
+            public static void showHistory() {
+                System.out.println("Back Stack    : " + back);
+                System.out.println("Current Page  : " + currentPage);
+                System.out.println("Forward Stack : " + forward);
+
+            }
 
         }
-        if (!PAGE.isEmpty()) {
-            back.push(currentPage);
-    }
-        forwared.clear();
-}
