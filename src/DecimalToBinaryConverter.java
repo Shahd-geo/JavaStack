78b9521ecc753617bbb64e394f4c79f692597cb3
Shahd <shahad9mubarak@gmail.com>
2026-04-26 09:07:53 +0400

    print binarry num

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -1,23 +1,27 @@
+
 public class DecimalToBinaryConverter {
     static void main(String[] args) {
-        int [] num={5,10,13,7,1,8};
-        for (int n :num){
-            System.out.println("decimal"+n);
+        int[] num = {5, 10, 13, 7, 1, 8};
+        for (int n : num) {
+            System.out.println("decimal" + n);
+            System.out.println("BINARY" + DecimalToBinary(n));
         }
+
     }
 
-}
-public static String  DecimalToBinaryConverter (int n){
-    Stack<Integer> STACK=new Stack<>();
-    while (n > 0){
-        int remainder = n %2;
-        STACK.push(remainder);
-        n=n/2;
-    }
-    String BINARY = "";
-    while (!STACK.isEmpty()){
-        BINARY+=STACK.pop();
-    }
-    return BINARY;
+    public static String DecimalToBinary(int n) {
+        Stack<Integer> STACK = new Stack<>();
+        while (n > 0) {
+            int remainder = n % 2;
+            STACK.push(remainder);
+            n = n / 2;
+        }
+        String BINARY = "";
+        while (!STACK.isEmpty()) {
+            BINARY += STACK.pop();
+        }
+        return BINARY;
 
+    }
 }
+
