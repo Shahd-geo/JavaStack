62a97917955cfbb49bae1209276df7aad4dbd75f
Shahd <shahad9mubarak@gmail.com>
2026-04-26 11:44:01 +0400

    Task 10: Validate HTML/XML Tags Using Java Stack

--- src/HtmlTagValidator.java
+++ src/HtmlTagValidator.java
@@ -2,30 +2,38 @@ import java.util.Stack;
 
 public class HtmlTagValidator {
     static void main(String[] args) {
+        System.out.println(validateHtmlTags("<div><p></p></div>"));
+            }
 
-}
-public static boolean validateHtmlTags(String html){
-        Stack<String>STACK=new Stack<>();
-        for (int i =0; i<html.length();i++){
-            if (html.charAt(i) == '<') {
-                int j = html.indexOf('>', i);
-                String tag = html.substring(i + 1, j);
+            public static boolean validateHtmlTags(String html) {
 
-                //if tag is close
-                if (tag.startsWith("/")) {
+            Stack<String> STACK = new Stack<>();
 
-                    String name = tag.substring(1);
+                for (int i = 0; i < html.length(); i++) {
 
-                    if (!STACK.isEmpty() || !STACK.pop().equals(name)) {
-                        return false;
+                    if (html.charAt(i) == '<') {
+
+                        int j = html.indexOf('>', i);
+                        String tag = html.substring(i + 1, j);
+
+                        if (tag.startsWith("/")) {
+
+                            String name = tag.substring(1);
+
+                            if (STACK.isEmpty() || !STACK.pop().equals(name)) {
+                                return false;
+                            }
+
+                        }
+                        else {
+                            STACK.push(tag);
+                        }
+
+                        i = j;
                     }
                 }
 
-
-
+                return STACK.isEmpty();
             }
         }
 
-}
-
-}
