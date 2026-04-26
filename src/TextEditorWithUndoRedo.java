c7900b24d177d84a63b668aa888180dfab018294
Shahd <shahad9mubarak@gmail.com>
2026-04-26 10:17:06 +0400

    build string builder to modify the same string

--- src/TextEditorWithUndoRedo.java
+++ src/TextEditorWithUndoRedo.java
@@ -3,6 +3,9 @@ import java.util.Stack;
 public class TextEditorWithUndoRedo {
     Stack<String>undo=new Stack<>();
     Stack<String>redo=new Stack<>();
+    StringBuilder text =new StringBuilder();
+
+
 
 
 }
