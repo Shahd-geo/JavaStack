97942cfbf011f157fb523cb43003bdc8616a2fce
Shahd <shahad9mubarak@gmail.com>
2026-04-26 09:09:20 +0400

    Task 7 : Convert Decimal to Binary Using Java Stack

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -1,10 +1,10 @@
-
+import java.util.Stack;
 public class DecimalToBinaryConverter {
     static void main(String[] args) {
         int[] num = {5, 10, 13, 7, 1, 8};
         for (int n : num) {
-            System.out.println("decimal" + n);
-            System.out.println("BINARY" + DecimalToBinary(n));
+            System.out.println("decimal : " + n);
+            System.out.println("BINARY : " + DecimalToBinary(n));
         }
 
     }
