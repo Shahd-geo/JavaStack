b3874d0248df228b57aabd907902825ef2550bd6
Shahd <shahad9mubarak@gmail.com>
2026-04-26 08:57:48 +0400

    CALCULATE REMAINDER

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -5,6 +5,10 @@ public static String  DecimalToBinaryConverter (int n){
     Stack<Integer> STACK=new Stack<>();
     while (n > 0){
         int remainder = n %2;
+        STACK.push(remainder);
+        n=n/2;
     }
+    String BINARY = "";
+    while (!)
 
 }
