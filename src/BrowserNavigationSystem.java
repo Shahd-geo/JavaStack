31c4768f02036e2e20f9dd42d7042954af2e99be
Shahd <shahad9mubarak@gmail.com>
2026-04-26 16:16:15 +0400

    ADD max var 10

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -3,5 +3,6 @@ import java.util.Stack;
 public class BrowserNavigationSystem {
     Stack<String>back=new Stack<>();
     Stack<String>forwared=new Stack<>();
-    String PAGE = "HOME";
+    String PAGE = "";
+    int max = 10;
 }
