ec9c27fb8be0dd0cb27842370e49eef4d8e4d93f
Shahd <shahad9mubarak@gmail.com>
2026-04-26 08:50:18 +0400

    Ass remainder var

--- src/DecimalToBinaryConverter.java
+++ src/DecimalToBinaryConverter.java
@@ -3,5 +3,8 @@ public class DecimalToBinaryConverter {
 }
 public static String  DecimalToBinaryConverter (int n){
     Stack<Integer> STACK=new Stack<>();
+    while (n > 0){
+        int remainder = n %2;
+    }
 
 }
