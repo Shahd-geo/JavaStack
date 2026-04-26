dd792843105dffedaacf802aad076dee377951f1
Shahd <shahad9mubarak@gmail.com>
2026-04-26 16:18:41 +0400

    CREAT VISITPAG FUNCTION

--- src/BrowserNavigationSystem.java
+++ src/BrowserNavigationSystem.java
@@ -6,4 +6,23 @@ public class BrowserNavigationSystem {
     String PAGE = "";
     int max = 10;
     public static void main(String[] args) {
+        visitPage("linkedIn.com");
+        visitPage("gitHub.com");
+        visitPage("zoom.com");
+        visitPage("googlemeet.com");
+
+        goBack();
+        goBack();
+        goForward();
+        visitPage("GEE.com");
+
+        goForward();
+        goBack();
+        showHistory();
+
+
+    }
+    static void visitPage(String url) {
+
+    }
 }
