f848952a8265c3f4f69037992396545235ee9ccd
Shahd <shahad9mubarak@gmail.com>
2026-04-26 10:56:54 +0400

    //if tag is close

--- src/HtmlTagValidator.java
+++ src/HtmlTagValidator.java
@@ -6,10 +6,21 @@ public class HtmlTagValidator {
 }
 public static boolean validateHtmlTags(String html){
         Stack<String>STACK=new Stack<>();
-        for (int i =0; i<str.length();i++){
+        for (int i =0; i<html.length();i++){
             if (html.charAt(i) == '<') {
                 int j = html.indexOf('>', i);
                 String tag = html.substring(i + 1, j);
+
+                //if tag is close
+                if (tag.startsWith("/")) {
+
+                    String name = tag.substring(1);
+
+                    if (!STACK.isEmpty() || !STACK.pop().equals(name)) {
+                        return false;
+                    }
+                }
+
 
 
             }
