f9d915ea3ab5c95ade4a9dc6eef28e1146b325ac
Shahd <shahad9mubarak@gmail.com>
2026-04-26 12:30:24 +0400

    add N =3

--- src/TowerOfHanoiSolver.java
+++ src/TowerOfHanoiSolver.java
@@ -2,11 +2,23 @@ import java.util.Stack;
 
 public class TowerOfHanoiSolver {
 
-     Stack<Integer> A = new Stack<>();
-     Stack<Integer> B = new Stack<>();
-     Stack<Integer> C = new Stack<>();
-     int move = 0;
+    public static void main(String[] args) {
+        Stack<Integer> A = new Stack<>();
+        Stack<Integer> B = new Stack<>();
+        Stack<Integer> C = new Stack<>();
+        int move = 0;
+        int n = 3;
+        for (int i = n; i >= 1; i--) {
+            A.push(i);
+        }
 
 
 
-}
+
+
+    }
+
+
+
+
+    }
