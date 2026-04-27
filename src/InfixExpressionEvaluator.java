78fb40f23ea076d0783ac207c47fbf836922392e
Shahd <shahad9mubarak@gmail.com>
2026-04-27 16:03:58 +0400

    add value to str

--- src/InfixExpressionEvaluator.java
+++ src/InfixExpressionEvaluator.java
@@ -9,8 +9,6 @@ public class InfixExpressionEvaluator {
 
 
 
-    }
-
 
 }
     public static int precedence(char op) {
@@ -39,8 +37,8 @@ public class InfixExpressionEvaluator {
         return 0;
     }
     public static int evaluate(String expr) {
-         Stack<Integer>  operand = new Stack<>();
-         Stack<Character>  operator = new Stack<>();
+        Stack<Integer> operand = new Stack<>();
+        Stack<Character> operator = new Stack<>();
         for (int i = 0; i < expr.length(); i++) {
             char ch = expr.charAt(i);
             if (ch == ' ') {
@@ -49,38 +47,17 @@ public class InfixExpressionEvaluator {
             if (Character.isDigit(ch)) {
                 int num = ch - '0';
                 operand.push(num);
-            }else if (ch == '(') {
+            } else if (ch == '(') {
                 operator.push(ch);
 
-            }
-            else if (ch == ')') {
+            } else if (ch == ')') {
                 while (operator.peek() != '(') {
                     int b = operand.pop();
                     int a = operand.pop();
                     operand.push(applyOp(a, b, operator.pop()));
                 }
                 return operand.pop();
-                }
-
-                }
-
-
-
-
-
-
             }
 
-
-
-
-
-
-
-
         }
-
-
-
-
     }
