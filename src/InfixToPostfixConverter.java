d67b76740374b2f1668036e328cf134731685b1c
Shahd <shahad9mubarak@gmail.com>
2026-04-27 09:48:11 +0400

    ADD precedence FUNCTION

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -44,20 +44,27 @@ public class InfixToPostfixConverter {
            return out.toString();
 
        }
-       public static Integer evaluatePostfix(String postfix) {
-
-
-           }
-
-
-
+       public static Integer evaluatePostfix(String STR) {
+           Stack<Integer> stack = new Stack<>();
+           for (int i = 0; i < STR.length(); i++) {
+               char ch = STR.charAt(i);
+               if (Character.isDigit(ch)) {
+                   stack.push(ch - '0');
+               } else if (ch == '+' || ch == '-' || ch == '*') {
+                   int a = stack.pop();
+                   int b = stack.pop();
+                   int result = 0;
+                   if (ch == '+') result = a + b;
+                   if (ch == '-') result = a - b;
+                   if (ch == '*') result = a * b;
+                   stack.push(result);
 
 
                }
-
-
-        }
-
+           }
+           return stack.pop();
+       }
+    public static int precedence(char op) {
 
     }
-}
+
