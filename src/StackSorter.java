1916b5a94a7c6153884cffbeccc185d2b9237985
Shahd <shahad9mubarak@gmail.com>
2026-04-27 14:42:50 +0400

    while (!stack.isEmpty())

--- src/StackSorter.java
+++ src/StackSorter.java
@@ -3,7 +3,10 @@ import java.util.Stack;
 public class StackSorter {
     public static void sortStack(Stack<Integer> stack) {
         Stack<Integer> temp = new Stack<>();
+        while (!stack.isEmpty()) {
 
-    }
+
+
+        }
 
     }
