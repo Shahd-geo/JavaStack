7e9f91736cc6c2ec059cad53095f5e22254b3ee2
Shahd <shahad9mubarak@gmail.com>
2026-04-27 12:09:14 +0400

    cheack minstack not empty

--- src/MinStack.java
+++ src/MinStack.java
@@ -4,6 +4,10 @@ public class MinStack {
     static Stack<Integer> minStack = new Stack<>();
     public static Integer push(int x) {
         mainStack.push(x);
+
+        if (minStack.isEmpty() || x <= minStack.peek()) {
+            minStack.push(x);
+        }
     }
 
 
