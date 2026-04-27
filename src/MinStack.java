82c674a3b5be1821db5bf44ff869e61ad3f82187
Shahd <shahad9mubarak@gmail.com>
2026-04-27 12:08:09 +0400

    add push function

--- src/MinStack.java
+++ src/MinStack.java
@@ -2,6 +2,7 @@ import java.util.Stack;
 public class MinStack {
     static Stack<Integer> mainStack = new Stack<>();
     static Stack<Integer> minStack = new Stack<>();
+    public static Integer push(int x) {
 
 
 }
