c9d7c579b9ad473789c2e7033a5ca269e1731114
Shahd <shahad9mubarak@gmail.com>
2026-04-27 10:08:47 +0400

    add findNextGreaterElement function

--- src/NextGreaterElementFinder.java
+++ src/NextGreaterElementFinder.java
@@ -1,4 +1,10 @@
 import java.util.Stack;
 
 public class NextGreaterElementFinder {
+    static void main(String[] args) {
+
+    }
+
+public static int[] findNextGreaterElement(int [] arr){
+    }
 }
