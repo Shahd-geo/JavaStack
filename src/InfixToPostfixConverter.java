77151497394c5af60512de40fb004bb39d82a182
Shahd <shahad9mubarak@gmail.com>
2026-04-27 09:22:38 +0400

    add ^ (exponent) highest

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -8,12 +8,16 @@ public class InfixToPostfixConverter {
     public static Integer precedence(char op){
         Stack<Character> ops = new Stack<>();
         StringBuilder output = new StringBuilder();
-        for (int i = 0; i< output.charAt(i);i++){
+        for (int i = 0; i< output.charAt(i);i++) {
             if (op == '+' || op == '-') {
                 return 1;
 
-            } if (op == '*' || op == '/' || op == '%')
+            }
+            if (op == '*' || op == '/' || op == '%')
                 return 2;
+            if (op == '^'){
+                return 3;
+            }
 
         }
 
