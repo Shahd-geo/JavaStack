9b1e0e0ffe3188bf91679c4cc8f57e46ea4c58a7
Shahd <shahad9mubarak@gmail.com>
2026-04-27 12:08:27 +0400

    mainStack.push(x);

--- src/MinStack.java
+++ src/MinStack.java
@@ -3,6 +3,8 @@ public class MinStack {
     static Stack<Integer> mainStack = new Stack<>();
     static Stack<Integer> minStack = new Stack<>();
     public static Integer push(int x) {
+        mainStack.push(x);
+    }
 
 
 }
