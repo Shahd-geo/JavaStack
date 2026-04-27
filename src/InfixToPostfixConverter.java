85eafd60db19e47a0303084374ecfb1a85dd7e59
Shahd <shahad9mubarak@gmail.com>
2026-04-27 09:28:01 +0400

    add infixToPostfix(String infix) - Convert infix to postfix

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -5,19 +5,22 @@ public class InfixToPostfixConverter {
 
 
     }
-    public static Integer precedence(char op){
-        Stack<Character> ops = new Stack<>();
-        StringBuilder output = new StringBuilder();
-        for (int i = 0; i< output.charAt(i);i++) {
-            if (op == '+' || op == '-') {
-                return 1;
+    public static Integer precedence(char op) {
+        if (op == '+' || op == '-') {
+            return 1;
 
-            }
-            if (op == '*' || op == '/' || op == '%')
-                return 2;
-            if (op == '^'){
-                return 3;
-            }
+        }
+        if (op == '*' || op == '/' || op == '%')
+            return 2;
+        if (op == '^') {
+            return 3;
+        }
+        return 0;
+    }
+       public static String infixToPostfix(String expr) {
+
+        }
+
 
         }
 
