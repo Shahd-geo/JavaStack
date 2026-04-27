ce4649f65816ec712afbcc7d0fa3e66032a03b73
Shahd <shahad9mubarak@gmail.com>
2026-04-27 09:43:17 +0400

    teruen to string

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -20,25 +20,29 @@ public class InfixToPostfixConverter {
        public static String infixToPostfix(String expr) {
            Stack<Character> ops = new Stack<>();
            StringBuilder out = new StringBuilder();
-           for (char ch : expr.toCharArray()){
+           for (char ch : expr.toCharArray()) {
                if (Character.isDigit(ch)) {
                    out.append(ch).append(" ");
-               }
-               else if (ch == '(') {
+               } else if (ch == '(') {
                    ops.push(ch);
-               }
-
-               else if (ch == ')') {
+               } else if (ch == ')') {
                    while (!ops.isEmpty() && ops.peek() != '(') {
                        out.append(ops.pop()).append(" ");
                    }
-               }else { // operator
-                       while (!ops.isEmpty() && precedence(ops.peek()) >= precedence(ch)) {
-                           out.append(ops.pop()).append(" ");
-                       }
-                       ops.push(ch);
+               } else { // operator
+                   while (!ops.isEmpty() && precedence(ops.peek()) >= precedence(ch)) {
+                       out.append(ops.pop()).append(" ");
                    }
+                   ops.push(ch);
                }
+               while (!ops.isEmpty()) {
+                   out.append(ops.pop()).append(" ");
+               }
+
+               return out.toString();
+           }
+
+       }
 
 
 
