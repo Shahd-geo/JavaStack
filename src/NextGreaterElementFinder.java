7432d355d08225af2e3959d2563a8f60a163c29b
Shahd <shahad9mubarak@gmail.com>
2026-04-27 10:28:05 +0400

    Task 14 – Find Next Greater Element Using StacK

--- src/NextGreaterElementFinder.java
+++ src/NextGreaterElementFinder.java
@@ -1,10 +1,12 @@
 import java.util.Arrays;
 import java.util.Stack;
 
+
 public class NextGreaterElementFinder {
     static void main(String[] args) {
         int[] arr = {4, 5, 2, 10};
         int[] result = findNextGreaterElement(arr);
+        System.out.println(Arrays.toString(findNextGreaterElement(arr)));
 
 
 
@@ -18,7 +20,7 @@ public static int[] findNextGreaterElement(int [] arr){
     Arrays.fill(result,-1);
     Stack<Integer> STACK=new Stack<>();
     for (int i = 0 ; i<n;i++){
-        while (!STACK.isEmpty() && arr[STACK.peek()]<arr[i]]{
+        while (!STACK.isEmpty() && arr[STACK.peek()]<arr[i]){
             int index = STACK.pop();
             result[index] = arr[i];
         }
