557acc1fabf24e3d0a7bbdb299d9c011a640acaa
Shahd <shahad9mubarak@gmail.com>
2026-04-27 09:50:40 +0400

    ADD precedence FUNCTION

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -65,12 +65,18 @@ public class InfixToPostfixConverter {
            return stack.pop();
        }
     public static int precedence(char op) {
-        if (op == '+' || op == '-') return 1;
-        if (op == '*' || op == '/') return 2;
-        if (op=='^' )  return 2;
-
-
+        if (op == '+' || op == '-'){
+            return 1;
+        if (op == '*' || op == '/'){
+            return 2;
+        }
+        if (op=='^' )  {
+            return 2;
     }
 
+
+        return 0;
     }
+
+
 
