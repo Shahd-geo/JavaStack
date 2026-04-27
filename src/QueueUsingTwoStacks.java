700aaf98c8568f9064dd560d12bc6684730ad731
Shahd <shahad9mubarak@gmail.com>
2026-04-27 10:48:08 +0400

    delet element

--- src/QueueUsingTwoStacks.java
+++ src/QueueUsingTwoStacks.java
@@ -4,10 +4,12 @@ public class QueueUsingTwoStacks {
     static Stack<Integer> s1 = new Stack<>();
     static Stack<Integer> s2 = new Stack<>();
 
-public static Integer Enqueue(int x) {
+public static int Enqueue(int x) {
     s1.push(x);
     System.out.println("Enqueue: " + x);
 }
+public static int dequeue() {
+
 
 
 }
