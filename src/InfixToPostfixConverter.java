5f9057489d61a1717702d49dc157ab57cd566d0a
Shahd <shahad9mubarak@gmail.com>
2026-04-27 09:52:15 +0400

    ADD precedence FUNCTION

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -5,6 +5,7 @@ public class InfixToPostfixConverter {
 
 
     }
+
     public static Integer precedence(char op) {
         if (op == '+' || op == '-') {
             return 1;
@@ -17,66 +18,57 @@ public class InfixToPostfixConverter {
         }
         return 0;
     }
-       public static String infixToPostfix(String expr) {
-           Stack<Character> ops = new Stack<>();
-           StringBuilder out = new StringBuilder();
-           for (char ch : expr.toCharArray()) {
-               if (Character.isDigit(ch)) {
-                   out.append(ch).append(" ");
-               } else if (ch == '(') {
-                   ops.push(ch);
-               } else if (ch == ')') {
-                   while (!ops.isEmpty() && ops.peek() != '(') {
-                       out.append(ops.pop()).append(" ");
-                   }
-               } else { // operator
-                   while (!ops.isEmpty() && precedence(ops.peek()) >= precedence(ch)) {
-                       out.append(ops.pop()).append(" ");
-                   }
-                   ops.push(ch);
-               }
-               while (!ops.isEmpty()) {
-                   out.append(ops.pop()).append(" ");
-               }
 
-               return out.toString();
-           }
-           return out.toString();
+    public static String infixToPostfix(String expr) {
+        Stack<Character> ops = new Stack<>();
+        StringBuilder out = new StringBuilder();
+        for (char ch : expr.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                out.append(ch).append(" ");
+            } else if (ch == '(') {
+                ops.push(ch);
+            } else if (ch == ')') {
+                while (!ops.isEmpty() && ops.peek() != '(') {
+                    out.append(ops.pop()).append(" ");
+                }
+            } else { // operator
+                while (!ops.isEmpty() && precedence(ops.peek()) >= precedence(ch)) {
+                    out.append(ops.pop()).append(" ");
+                }
+                ops.push(ch);
+            }
+            while (!ops.isEmpty()) {
+                out.append(ops.pop()).append(" ");
+            }
 
-       }
-       public static Integer evaluatePostfix(String STR) {
-           Stack<Integer> stack = new Stack<>();
-           for (int i = 0; i < STR.length(); i++) {
-               char ch = STR.charAt(i);
-               if (Character.isDigit(ch)) {
-                   stack.push(ch - '0');
-               } else if (ch == '+' || ch == '-' || ch == '*') {
-                   int a = stack.pop();
-                   int b = stack.pop();
-                   int result = 0;
-                   if (ch == '+') result = a + b;
-                   if (ch == '-') result = a - b;
-                   if (ch == '*') result = a * b;
-                   stack.push(result);
-
-
-               }
-           }
-           return stack.pop();
-       }
-    public static int precedence(char op) {
-        if (op == '+' || op == '-'){
-            return 1;
-        if (op == '*' || op == '/'){
-            return 2;
+            return out.toString();
         }
-        if (op=='^' )  {
-            return 2;
+        return out.toString();
+
     }
 
+    public static Integer evaluatePostfix(String STR) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < STR.length(); i++) {
+            char ch = STR.charAt(i);
+            if (Character.isDigit(ch)) {
+                stack.push(ch - '0');
+            } else if (ch == '+' || ch == '-' || ch == '*') {
+                int a = stack.pop();
+                int b = stack.pop();
+                int result = 0;
+                if (ch == '+') result = a + b;
+                if (ch == '-') result = a - b;
+                if (ch == '*') result = a * b;
+                stack.push(result);
 
-        return 0;
+
+            }
+        }
+        return stack.pop();
     }
+}
+
 
 
 
