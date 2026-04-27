d27b6d0ed047eb432b226ff720a0cbddcca68dd8
Shahd <shahad9mubarak@gmail.com>
2026-04-27 15:13:18 +0400

    CREAT precedence FUNCTION

--- src/InfixExpressionEvaluator.java
+++ src/InfixExpressionEvaluator.java
@@ -3,6 +3,8 @@ import java.util.Stack;
 public class InfixExpressionEvaluator {
     static Stack<Double>  operand = new Stack<>();
     static Stack<Character>  operator = new Stack<>();
+    public static int precedence(char op) {
 
 
-}
+
+    }
