9b11a3a43c57e4484ffcc4958bec4fe8b2ad318a
Shahd <shahad9mubarak@gmail.com>
2026-04-28 08:53:48 +0400

    add stack

--- src/StackSorter.java
+++ src/StackSorter.java
@@ -1,14 +1,15 @@
 import java.util.Stack;
 
 public class StackSorter {
-    static void main(Strin= new Stack<>();
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
         s.push(3);
         s.push(1);
         s.push(4);
-        s.push(2);g[] args) {
-            Stack<Integer> s
-            System.out.println("orginal stack:" + s);
-        System.out.println(sortStack(s));
+        System.out.println("Before :");
+
+
+        Stack<Integer
 
 
     }
