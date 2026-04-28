18d3ca548ae29efe5387b9b617a7b40d1f520056
Shahd <shahad9mubarak@gmail.com>
2026-04-28 09:44:34 +0400

    Task 18: Evaluate Infix Expression Directly Using Two Stacks

--- src/InfixExpressionEvaluator.java
+++ src/InfixExpressionEvaluator.java
@@ -1,66 +1,72 @@
-//import java.util.Stack;
+import java.util.Stack;
 
-//public class InfixExpressionEvaluator {
-    //static void main(String[] args) {
-       // String expr = "3+4*2";
-       // int result = evaluate(expr);
-//System.out.println("Result: " + result);
+public class InfixExpressionEvaluator {
+    public static void main(String[] args) {  // fixed: added public
+        String expr = "3+4*2";
+        int result = evaluate(expr);
+        System.out.println("Result: " + result);
+    }
 
+    public static int precedence(char op) {
+        if (op == '+' || op == '-') {
+            return 1;
+        }
+        if (op == '*' || op == '/') {
+            return 2;
+        }
+        return 0;
+    }
 
+    public static int applyOp(int a, int b, char op) {
+        if (op == '+') return a + b;
+        if (op == '-') return a - b;
+        if (op == '*') return a * b;
+        if (op == '/') return a / b;
+        return 0;
+    }
 
+    public static int evaluate(String expr) {
+        Stack<Integer> operand = new Stack<>();
+        Stack<Character> operator = new Stack<>();
 
-   // }
+        for (int i = 0; i < expr.length(); i++) {
+            char ch = expr.charAt(i);
 
-    //public static int precedence(char op) {
-//if (op == '+' || op == '-') {
-           // return 1;
-       // }
-      //  if (op == '*' || op == '/') {
-          //  return 2;
-      //  }
-      //  return 0;
+            if (ch == ' ') continue;
 
+            if (Character.isDigit(ch)) {
+                int num = ch - '0';
+                operand.push(num);
+            }
+            else if (ch == '(') {
+                operator.push(ch);
+            }
+            else if (ch == ')') {
+                while (operator.peek() != '(') {
+                    int b = operand.pop();
+                    int a = operand.pop();
+                    operand.push(applyOp(a, b, operator.pop()));
+                }
+                operator.pop(); // remove '('
+            }
+            else { // operator case (+, -, *, /)
+                while (!operator.isEmpty() &&
+                        precedence(operator.peek()) >= precedence(ch)) {
+                    int b = operand.pop();
+                    int a = operand.pop();
+                    operand.push(applyOp(a, b, operator.pop()));
+                }
+                operator.push(ch);
+            }
+        }
 
-  //  }
+        // apply remaining operators
+        while (!operator.isEmpty()) {
+            int b = operand.pop();
+            int a = operand.pop();
+            operand.push(applyOp(a, b, operator.pop()));
+        }
 
-   // public static int applyOp(int a, int b, char op) {
-      //  if (op == '+') {
-           // return a + b;
-       // }
-       // if (op == '-') {
-            //return a - b;
-        //}
-     //   if (op == '*') {
-           // return a * b;
-        //}
-        //if (op == '/') {
-           // return a / b;
-       // }
-       // return 0;
-    //}
-
-   // public static int evaluate(String expr) {
-      //  Stack<Integer> operand = new Stack<>();
-       // Stack<Character> operator = new Stack<>();
-      //  for (int i = 0; i < expr.length(); i++) {
-           // char ch = expr.charAt(i);
-          //  if (ch == ' ') {
-                //continue;
-          //  }
-          //  if (Character.isDigit(ch)) {
-             //   int num = ch - '0';
-              //  operand.push(num);
-            //} else if (ch == '(') {
-             //   operator.push(ch);
-
-         //   } else if (ch == ')') {
-              //  while (operator.peek() != '(') {
-                   // int b = operand.pop();
-                  //  int a = operand.pop();
-                   // operand.push(applyOp(a, b, operator.pop()));
-              //  }
-              //  return operand.pop();
-           // }
-
-   // }
-//}
+        return operand.pop();
+    }
+}
