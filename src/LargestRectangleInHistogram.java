2e8409d6d3f75ad48a097f8724199de265070536
Shahd <shahad9mubarak@gmail.com>
2026-04-28 09:59:22 +0400

    add loop

--- src/LargestRectangleInHistogram.java
+++ src/LargestRectangleInHistogram.java
@@ -12,6 +12,31 @@ public class LargestRectangleInHistogram {
         Stack<Integer> stack = new Stack<>();
         int maxArea = 0;
         for (int i = 0; i <= heights.length; i++) {
+            int currentHeight = (i == heights.length) ? 0 : heights[i];
+
+            while (!stack.isEmpty() && currentHeight < heights[stack.peek()]) {
+
+                int h = heights[stack.pop()];
+
+                int width;
+                if (stack.isEmpty()) {
+                    width = i;
+                } else {
+                    width = i - stack.peek() - 1;
+                }
+
+                int area = h * width;
+                maxArea = Math.max(maxArea, area);
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+        }
+
+        }
 
 
 
